08d4851896a4341ac2afb67ee8bd6efdf77aa333
AprajitaChhawi <achhawip@gmail.com>
2021-04-03 19:54:15 +0530

    Create Day 3 digital root of a number.py

---
+++ Day 3 digital root of a number.py
@@ -0,0 +1,39 @@
+#User function Template for python3
+
+class Solution:
+    def digitalRoot(self, n):
+        if n<=9:
+            return n
+        ob=Solution()
+        return ob.digitalRoot((n%10)+(n//10))
+        '''
+        :param n: given number 
+        :return: digital root as defined
+        '''
+        # code here
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+    
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        print(Solution().digitalRoot(n))
+# } Driver Code Ends
