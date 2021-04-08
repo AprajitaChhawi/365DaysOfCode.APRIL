641b6fb5c16b2549f4a5d6f625e8f0d09a3acaf5
AprajitaChhawi <achhawip@gmail.com>
2021-04-09 00:03:07 +0530

    Create Day 8 array insert at end.py

---
+++ Day 8 array insert at end.py
@@ -0,0 +1,43 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+# You only need to insert the given element at 
+# the end, i.e., at index sizeOfArray - 1. You may 
+# assume that the array already has sizeOfArray - 1
+# elements. 
+def insertAtEnd(arr,sizeOfArray,element):
+    arr.append(element)
+    return arr
+    ##Your code here
+
+
+#{ 
+#Driver Code Starts.
+
+
+def main():
+    testcases=int(input())
+    
+    while(testcases>0):
+        sizeOfArray=int(input())
+        
+        arr=[int(x) for x in input().strip().split()]
+        
+        element=int(input())
+        
+        insertAtEnd(arr,sizeOfArray,element)
+        
+        for i in arr:
+            print(i,end=" ")
+        print()
+        
+        testcases-=1
+    
+
+if __name__=="__main__":
+    main()
+#} Driver Code Ends
